9d332b94845b8a97b10866a318e311885416ac16
Manuel Alabor <msites@msites.net>
2013-04-17 12:04:52 +0200

    moved mixin loading into its own file. yay

--- index.js
+++ index.js
@@ -41,14 +41,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
-
-var Backbone = require('backbone')
-	, _ = require('underscore')
-	, path = require('path')
-	, mixins = loadMixins()
+var barefootUtil = require('./util')
+	, mixins = barefootUtil.loadMixins()
 	, Router = require('./router')(mixins.RouterMixin)
-	, View = require('./view')(mixins.ViewMixin)
+	, View = require('./view')(mixins.ViewMixin);
 
 /* start
  * Starts your application using the given router.
@@ -69,64 +65,3 @@ module.exports = {
 	, View: View
 	, start: start
 };
-
-
-/* mergeObjectProperties
- * This function takes two objects and uses underscores extend function to
- * merge each object contained inside of them.
- *
- * > var A = { person: { name: 'Fritz' } };
- * > var B = { person: { surname: 'Fritzenson' }, city: 'Fritzhausen' };
- * > var merged = mergeObjectProperties(A, B);
- * > // { person: { name: 'Fritz', surname: 'Fritzenson' }, city: 'Fritzhausen'}
- *
- * Parameters:
- *     (Object) objectA
- *     (Object) objectB
- *
- * Returns:
- *     A merged object
- */
-function mergeObjectProperties(objectA, objectB) {
-	var keys = _.keys(objectA);
-
-	_.each(keys, function(key) {
-		if(_.has(objectA, key)) {
-			_.extend(objectA[key], objectB[key]);
-		} else {
-			objectA[key] = objectB[key];
-		}
-	});
-
-	return objectA;
-}
-
-/* loadMixins
- * Since Barefoot is runnable on server and client, this function returns 
- * environment specific code read from the "server", "client" and "shared"
- * folders.
- * 
- * If there is shared code available these it gets merged with the specific
- * ones.
- *
- * Returns:
- *     A mixin to give a Backbone object barefoot capabilities.
- */
-function loadMixins() {
-	var specific
-		, shared = require('./shared');
-
-	if(process.browser) {
-		specific = require('./client');
-	} else {
-		specific = require('./server');
-	}
-
-	if(!_.isUndefined(shared) && !_.isUndefined(specific)) {
-		specific = mergeObjectProperties(specific, shared);
-	} else if(!_.isUndefined(shared) && _.isUndefined(specific)) {
-		specific = shared;
-	}
-
-	return specific;
-}
---
+++ util.js
@@ -0,0 +1,71 @@
+/** Class: Barefoot.Util
+ * Contains helper functions for loading environment specific mixins.
+ */
+var Backbone = require('backbone')
+	, _ = require('underscore')
+	, path = require('path');
+
+/** PrivateFunction: mergeObjectProperties
+ * This function takes two objects and uses underscores extend function to
+ * merge each object contained inside of them.
+ *
+ * > var A = { person: { name: 'Fritz' } };
+ * > var B = { person: { surname: 'Fritzenson' }, city: 'Fritzhausen' };
+ * > var merged = mergeObjectProperties(A, B);
+ * > // { person: { name: 'Fritz', surname: 'Fritzenson' }, city: 'Fritzhausen'}
+ *
+ * Parameters:
+ *     (Object) objectA
+ *     (Object) objectB
+ *
+ * Returns:
+ *     A merged object
+ */
+function mergeObjectProperties(objectA, objectB) {
+	var keys = _.keys(objectA);
+
+	_.each(keys, function(key) {
+		if(_.has(objectA, key)) {
+			_.extend(objectA[key], objectB[key]);
+		} else {
+			objectA[key] = objectB[key];
+		}
+	});
+
+	return objectA;
+}
+
+/** Function: loadMixins
+ * Since Barefoot is runnable on server and client, this function returns 
+ * environment specific code read from the "server", "client" and "shared"
+ * folders.
+ * 
+ * If there is shared code available these it gets merged with the specific
+ * ones.
+ *
+ * Returns:
+ *     A mixin to give a Backbone object barefoot capabilities.
+ */
+function loadMixins() {
+	var specific
+		, shared = require('./shared');
+
+	if(process.browser) {
+		specific = require('./client');
+	} else {
+		specific = require('./server');
+	}
+
+	if(!_.isUndefined(shared) && !_.isUndefined(specific)) {
+		specific = mergeObjectProperties(specific, shared);
+	} else if(!_.isUndefined(shared) && _.isUndefined(specific)) {
+		specific = shared;
+	}
+
+	return specific;
+}
+
+
+module.exports {
+	loadMixins: loadMixins
+}
