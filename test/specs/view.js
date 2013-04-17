0cbf748073aa3bc2faa6908993bf605170d52cbe
Manuel Alabor <msites@msites.net>
2013-04-17 18:27:31 +0200

    better mixin test

--- test/specs/view.js
+++ test/specs/view.js
@@ -1,11 +1,11 @@
 describe('View', function() {
 	describe('Mixin Functionality', function() {
-		it('should apply a mixin', function() {
+		it('should apply a mixin to its prototype', function() {
 			var mixin = { mix: function() { } }
 				, View = requireModule('./view')
 				, withMixin = View(mixin);
 
-			withMixin.prototype.mix.should.be.equal(mixin.mix);
+			withMixin.prototype.should.have.property('mix', mixin.mix);
 		});
 	})
 })
