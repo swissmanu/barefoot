fad00c8649b1635537ba66e4c2408e02fb80add9
Manuel Alabor <msites@msites.net>
2013-04-17 11:25:50 +0200

    better style for docs, added license

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 docs:
 	-mkdir ./docs
-	@NaturalDocs -i ./ -o HTML ./docs -p ./.naturaldocs -xi ./node_modules
+	@NaturalDocs -i ./ -o HTML ./docs -p ./.naturaldocs -xi ./node_modules -s Default style
 
 .PHONY: docs
--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 /** Title: Barefoot
- * Barefoot makes it possible to render and run a Backbone application on a
- * node.js server. To accomplish this task, barefoot adds a few mixins to 
- * Backbones base objects.
+ * Barefoot makes code sharing between browser and server reality. Write your
+ * application once and run it on both ends of the wire.
+ *
+ * It builds upon the popular Backbone.JS library and keeps its own additions as
+ * unobtrusive as possible.
+ *
+ * Server side execution is accomplished by using Node.JS and Express.JS. Once
+ * delivered to the users browser, no additional JavaScript libraries are
+ * needed when configured properly.
+ *
+ * If you implement your views with care, your client side follows the
+ * "Unobtrusive JavaScript" principle
+ * (http://roca-style.org/#unobtrusive-javascript) completely.
  *
  * Author:
- *     Manuel Alabor <manuel@alabor.me>
+ * * Manuel Alabor, <manuel@alabor.me>
+ *
+ * Source on Github:
+ * * https://github.com/swissmanu/barefoot
+ *
+ *
+ * License:
+ * Copyright (c) 2013 Manuel Alabor
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 
