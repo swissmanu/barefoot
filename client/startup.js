a119a66bc21894db00e82bfd87e47da76aeb2321
Manuel Alabor <msites@msites.net>
2013-04-17 11:42:31 +0200

    added startup functions to the documentation

--- client/startup.js
+++ client/startup.js
@@ -1,6 +1,24 @@
+/** Mixin: Barefoot.Startup.Client
+ * This startup mixin supplies a client specific startup function to get the
+ * application up and running in the browser.
+ */
 var Backbone = require('backbone');
 
-
+/** Function: startup
+ * Ensures that Backbone is available globally on the client. It further starts
+ * up the Backbone.history.
+ *
+ * Finally an instance of the passed Router object is created. The startOptions
+ * object is used to create the Router.
+ *
+ * Parameters:
+ *     (Barefoot.Router) Router - A <Barefoot.Router> object. Important: This is
+ *                                is not an instance of the router, it is the 
+ *                                "plain" object which can be used for running
+ *                                "new Router(...)".
+ *     (Object) startOptions - This object literal is passed to the router when
+ *                             initalizing it.
+ */
 function startup(Router, startOptions) {
 
 	$(function() {
@@ -15,8 +33,6 @@ function startup(Router, startOptions) {
 
 		var concreteClientRouter = new Router(startOptions);
 		concreteClientRouter.start();
-
-		console.log('startup done');
 	});
 
 }
--- server/startup.js
+++ server/startup.js
@@ -1,5 +1,22 @@
+/** Mixin: Barefoot.Startup.Server
+ * This startup mixin supplies a server specific startup function to get the
+ * application up and running on the node.js server backend.
+ */
 var _ = require('underscore');
 
+/** Function: startup
+ * This function simply creates the <Barefoot.Router> prepared with the server
+ * side mixins. Before creating an instance of it, startOptions is checked if 
+ * an Express.JS app is present (if not, an error gets thrown).
+ *
+ * Parameters:
+ *     (Barefoot.Router) Router - A <Barefoot.Router> object. Important: This is
+ *                                is not an instance of the router, it is the 
+ *                                "plain" object which can be used for running
+ *                                "new Router(...)".
+ *     (Object) startOptions - This object literal is passed to the router when
+ *                             initalizing it.
+ */
 function startup(Router, startOptions) {
 	if(!_.has(startOptions, 'app')) {
 		throw new Error('Missing "app" property in "startOptions"! Please pass '
