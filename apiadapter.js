0ad3c245b5124782d7a824729a35e9b39053e3e1
Manuel Alabor <msites@msites.net>
2013-04-21 15:35:40 +0200

    yay. documentation

--- apiadapter.js
+++ apiadapter.js
@@ -1,6 +1,48 @@
 /** Class: Barefoot.APIAdapter
+ * When building a fancy JavaScript based client you'll probably have a straight
+ * forward REST API in the backend. In reality, Backbone builds heavily on this
+ * architectural pattern and allows you to bind any model or collection to a
+ * RESTful web service using JSON as data transfer format.
+ *
+ * Taking this architecture it comes clear that you need some code for your
+ * web service on the server which delivers your data and takes fresh data from
+ * the client. Now the most common way could be a simple Express.JS app
+ * implementation where you hard-code every REST resource. Since barefoot makes
+ * it easy to render your backbone views on the server, which probably will run
+ * your API too, this would not work quite satisfying anymore, right?
+ *
+ * Thats the point where APIAdapter joins the game: It binds a bunch of API 
+ * resources (actually just independant callback functions, each identified with
+ * a unique URI) and wraps it into Express.JS routes to make them available to
+ * the clients. During startup (<Barefoot.Startup.Server>), backbones sync
+ * function is automatically replaced with barefoots one to ensure that local
+ * API calls on the server are routed directly to the callbacks instead going 
+ * a detour via HTTP/AJAX. For further details, have a look into APIAdapters
+ * server mixin: <Barefoot.APIAdapter.Server>.
+ *
+ * You will probably touch the APIAdapter consciously only once: In your app
+ * factory on the server, you have to specify your desired API resources.
+ * Everything else will run behind the curtains afterwards.
+ *
+ * Example API Routes:
+ *
+ * > var contacts = [ { 'name': 'Bob' }, { 'name': 'Alice' } ];
+ * > var apiRoutes = {
+ * >     'get': {
+ * >         '/contacts': function() {
+ * >             return contacts;
+ * >         }
+ * >     }
+ * >     , 'post': {
+ * >         '/contacts': function(contact) {
+ * >             contacts.push(contact);
+ * >         }
+ * >     }
+ * >  };
+ *
+ * For more examples, see <Barefoot.APIAdapter.Server.createPlainRoutes> and
+ * <Barefoot.APIAdapter.Server.createContentRoutes>.
  */
-
 var _ = require('underscore')
 	, Backbone = require('backbone');
 
--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 module.exports = {
 	RouterMixin: require('./router-mixin')
 	, ViewMixin: require('./view-mixin')
-	, APIAdapterMixin: require('./apiadapter-mixin')
 	, startup: require('./startup')
 };
