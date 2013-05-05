7979951cac1bb8fad8ee9cd6a49e715dcf0ffbd7
Manuel Alabor <msites@msites.net>
2013-05-05 14:30:34 +0200

    fixed events export

--- test/specs/barefoot.js
+++ test/specs/barefoot.js
@@ -1,7 +1,14 @@
 var BarefootServer = global.BarefootServer
-	, BarefootClient = global.BarefootClient;
+	, BarefootClient = global.BarefootClient
+	, Backbone = require('backbone');
 
 describe('Barefoot', function() {
+	describe('Module exports', function() {
+		it('should backbones Events object as Events', function() {
+			Barefoot.Events.should.be.equal(Backbone.Events);
+		})
+	})
+	
 	describe('isRunningOnServer', function() {
 		it('should return true when barefoot is running on the server', function() {
 			BarefootServer.isRunningOnServer().should.be.true;
